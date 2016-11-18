77e5f0a08ff5418dd90c2f99d52d4396384ef390
Luccas Barbosa <luccasbarbosa@outlook.com>
2016-11-18 00:29:19 -0200

    Adicionando $http

---
+++ js/contatos.js
@@ -0,0 +1,24 @@
+[
+    {
+    	"nome":"Lucas Barbosa", 
+    	"telefone": "99999-9988", 
+    	"data": "2016-11-18T02:11:10.792Z", 
+    	"operadora":{
+    		"categoria": "Celular",
+            "nome": "OI",
+            "codigo": "14"
+    	}
+    },
+
+    {
+    	"nome":"Silvia Barbosa", 
+    	"telefone": "2016-04-16", 
+    	"data": "2016-11-18T02:11:10.792Z", 
+    	"operadora":{
+    		"categoria": "Celular",
+            "nome": "Vivo",
+            "codigo": "15"
+    	}
+    }
+    
+]
---
+++ js/operadoras.js
@@ -0,0 +1,32 @@
+[
+	{
+		"nome": "Vivo", 
+		"codigo": "15", 
+		"categoria": "Celular", 
+		"preco": "1"
+	},
+    {
+    	"nome": "Claro", 
+    	"codigo": "23", 
+    	"categoria": "Celular", 
+    	"preco": "2"
+    },
+    {
+    	"nome": "Tim", 
+    	"codigo": "41", 
+    	"categoria": "Celular", 
+    	"preco": "1"
+    },
+    {
+    	"nome": "Oi", 
+    	"codigo": "24", 
+    	"categoria": "Fixo", 
+    	"preco": "3"
+    },
+    {
+    	"nome": "Embratel", 
+    	"codigo": "21", 
+    	"categoria": "Fixo", 
+    	"preco": "1"
+    }
+]                    
